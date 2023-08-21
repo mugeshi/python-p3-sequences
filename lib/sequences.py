236bad7714ea8e1b47d600f117f66990494eb7d7
mugeshi <nicolemugeshi@gmail.com>
2023-08-21 22:01:45 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    a, b = 0, 1
+    while len(fibonacci_sequence) < length:
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+    print(fibonacci_sequence)
